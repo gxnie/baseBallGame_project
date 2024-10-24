6bab3a92dc03cdeccadeff22d30f193600954fa3
gxnie <wlsdl7520@naver.com>
2024-10-24 19:03:52 +0900

    feat. 기본 게임 요구사항 1레벨

---
+++ src/Lv1/Main.java
@@ -0,0 +1,12 @@
+package Lv1;
+
+import Lv1.BaseBallGame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BaseBallGame baseBallGame = new BaseBallGame();
+
+        baseBallGame.gamePlay();
+    }
+}
